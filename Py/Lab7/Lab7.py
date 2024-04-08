3672d09b860ec851c149828263b044cc1f42f928
Volodymyr Kozhukhar <kozhukharvladimir@gmail.com>
2024-04-08 14:41:42 +0300

    lab 7

---
+++ Py/Lab7/Lab7.py
@@ -0,0 +1,12 @@
+# -*- coding: cp1251 -*-
+s = [2, 5, 9, 14, 20, 27]
+print(f"Список s = {s}\n\n")
+tmp = s.copy()
+for i in range(len(tmp)):
+    tmp[i] = tmp[i] ** 2
+print(f"7.1 Квадрати списку s: {tmp}")
+tmp.clear()
+tmp = s.copy()
+for i in range(len(tmp)):
+    tmp[i] = tmp[i] % 11
+print(f"Залишки від ділення на 11, елементів s: {tmp}")
